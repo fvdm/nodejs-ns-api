3484d1e27f1d73162fb9f0f979e92f6fa4c6ed2e
Franklin van de Meent <fr@nkl.in>
2013-01-03 18:45:39 +0100

    Code: added talk() with basic code

--- nsapi.js
+++ nsapi.js
@@ -1,8 +1,39 @@
 var http = require('http'),
     app = {}
 
-
-
+// ! Communicate
+app.talk = function( path, props, callback ) {
+	if( typeof props === 'function' ) {
+		var callback = props
+		var props = {}
+	}
+	
+	var options = {
+		host:	'webservices.ns.nl',
+		port:	80,
+		path:	'/ns-api-'+ path +'?'+ querystring.stringify( props ),
+		method:	'GET'
+	}
+	
+	var req = http.request( options )
+	
+	req.on( 'response', function( response ) {
+		var data = ''
+		response.on( 'data', function( ch ) { data += ch })
+		response.on( 'close', function() { callback( new Error('disconnected') ) })
+		response.on( 'end', function() {
+			
+		})
+	})
+	
+	req.on( 'error', function( error ) {
+		var err = new Error('request')
+		err.details = error
+		callback( err )
+	})
+	
+	req.end()
+}
 
 // ready
 module.exports = app
