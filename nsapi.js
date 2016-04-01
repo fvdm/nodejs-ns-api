5f076bf8de92e585a39f419e8ee49d041921ccea
Franklin van de Meent <fr@nkl.in>
2016-04-01 18:41:10 +0200

    Major code rewrite, lots of fixes

--- nsapi.js
+++ nsapi.js
@@ -8,231 +8,417 @@ License:    Unlicense (Public Domain)
             (see UNLICENSE file or https://raw.github.com/fvdm/nodejs-ns-api/master/UNLICENSE)
 */
 
+var zlib = require ('zlib');
 var http = require ('httpreq');
 var parsexml = require ('nodexml') .xml2obj;
-var app = {username: '', password: ''};
+
+var config = {
+  username: '',
+  password: '',
+  timeout: 5000
+};
 
 
+/**
+ * Strip a key from an object
+ *
+ * @param obj {object} - Object to alter
+ * @param key {string} - Key to remove
+ * @returns {object}
+ */
 
-// Strip a key from object
 function objectOmit (obj, key) {
+  var i;
+
   if (obj instanceof Object) {
-    for (k in obj) {
-      if (k === key) {
-        delete obj [k];
+    for (i in obj) {
+      if (i === key) {
+        delete obj [i];
       } else {
-        obj [k] = objectOmit (obj [k], key);
+        obj [i] = objectOmit (obj [i], key);
       }
     }
-  }
-  else if (obj instanceof Array && obj.length >= 1) {
-    var i;
+  } else if (obj instanceof Array && obj.length) {
     for (i = 0; i < obj.length; i++) {
       obj [i] = objectOmit (obj [i], key);
     }
   }
+
   return obj;
 }
 
 
-// ! Vertrektijden
-app.vertrektijden = function (station, callback) {
-  app.talk ('avt', { station: station }, function (err, data) {
-    if (!err) {
-      if (!data.ActueleVertrekTijden || !data.ActueleVertrekTijden.VertrekkendeTrein) {
-        callback (new Error ('unexpected response' ))
-      } else {
-        data = data.ActueleVertrekTijden.VertrekkendeTrein;
-        if (data.RitNummer) {
-          data = [data];
-          callback (null, data);
-        } else {
-          var t;
-          for (t in data) {
-            data [t] .VertrekSpoorWijziging = data [t] .VertrekSpoor.wijziging;
-            data [t] .VertrekSpoor = data [t] .VertrekSpoor.$t;
-            delete data [t] .VertrekSpoor.$t;
-          }
-          callback (null, data);
-        }
-      }
-    } else {
-      callback (err, data);
-    }
-  });
-};
+/**
+ * Process talk() response
+ *
+ * @param err {Error,null} - httpreq error
+ * @param res {object} - Response resource
+ * @param callback {function} - `function (err, data) {}`
+ * @returns {void}
+ */
 
+function processResponse (err, res, callback) {
+  var data = res && res.body || '';
+  var error = null;
 
-// ! Prijzen
-app.prijzen = function (props, callback) {
-  app.talk ('prijzen-v2', props, callback);
-};
-
-
-// ! Reisadviezen
-app.reisadvies = function (props, callback) {
-  app.talk ('treinplanner', props, function (err, data) {
-    if (!err) {
-      if (!data.ReisMogelijkheden || !data.ReisMogelijkheden.ReisMogelijkheid) {
-        callback (new Error ('unexpected response'));
-      } else {
-        data = data.ReisMogelijkheden.ReisMogelijkheid;
-
-        if (!(data instanceof Array)) {
-          data = [data];
-        }
-
-        if (data.length >= 1) {
-          for (var r in data) {
-            var reis = data [r];
-            if (!(reis.ReisDeel instanceof Array)) {
-              reis.ReisDeel = [reis.ReisDeel];
-            }
-
-            for (var d in reis.ReisDeel) {
-              var deel = reis.ReisDeel [d];
-              for (var s in deel.ReisStop) {
-                var stop = deel.ReisStop [s];
-                if (stop.Spoor) {
-                  stop.SpoorWijziging = stop.Spoor.wijziging;
-                  stop.Spoor = stop.Spoor.$t;
-                  delete stop.Spoor.$t;
-                  deel.ReisStop [s] = stop;
-                }
-              }
-              reis.ReisDeel [d] = deel;
-            }
-
-            data [r] = reis;
-          }
-        }
-
-        callback (null, data);
-      }
-    } else {
-      callback (err, data);
-    }
-  });
-};
-
-
-// ! Stationslijst
-app.stations = function (treeKey, callback) {
-  if (typeof treeKey === 'function') {
-    var callback = treeKey;
-    var treeKey = 'code';
+  // request error
+  if (err) {
+    error = new Error ('request failed');
+    error.error = err;
+    callback (error);
+    return;
   }
 
-  app.talk ('stations-v2', function (err, data) {
-    if (!err) {
-      if (!data.Stations.Station) {
-        callback (new Error ('unexpected response'));
-      } else {
-        data = data.Stations.Station;
-        var tree = {};
-        for (var s in data) {
-          var station = data [s];
-
-          if (treeKey === 'code') {
-            tree [station.Code] = station;
-          } else if (typeof station [treeKey] !== 'string') {
-            callback (new Error ('key not found in station'));
-            return;
-          } else {
-            if (!tree [station [treeKey]]) {
-              tree [station [treeKey]] = {};
-            }
-            tree [station [treeKey]] [station.Code] = station;
-          }
-        }
-
-        callback (null, tree);
-      }
-    } else {
-      callback (err, data);
-    }
-  });
-};
-
-
-// ! Storingen
-app.storingen = function (params, callback) {
-  app.talk ('storingen', params, function (err, data) {
-    if (!err) {
-      if (!data.Storingen.Ongepland || !data.Storingen.Gepland) {
-        callback (new Error ('unexpected response'));
-      } else {
-        data = data.Storingen;
-        data.Ongepland = data.Ongepland.Storing || [];
-        data.Gepland = data.Gepland.Storing || [];
-
-        if (!(data.Ongepland instanceof Array)) {
-          data.Ongepland = [data.Ongepland];
-        }
-
-        if (!(data.Gepland instanceof Array)) {
-          data.Gepland = [data.Gepland];
-        }
-
-        callback (null, data);
-      }
-    } else {
-      callback (err, data);
-    }
-  });
-};
-
-
-// ! Communicate
-app.talk = function (path, props, callback) {
-  if (typeof props === 'function') {
-    callback = props;
-    props = {};
+  // gzip decoding
+  if (res.headers ['content-encoding'] === 'gzip') {
+    data = zlib.gunzipSync (data) .toString ('utf8');
   }
 
-  var url = 'https://webservices.ns.nl/ns-api-'+ path;
+  data = data.replace (/&#039;/g, '\'');
+
+  // parse xml
+  try {
+    data = parsexml (data);
+    data = objectOmit (data, '@');
+  } catch (e) {
+    error = new Error ('invalid response');
+    error.body = data;
+
+    callback (error);
+    return;
+  }
+
+  // parse API error
+  if (data.error) {
+    error = new Error ('API error');
+    error.api = data.error;
+
+    callback (error);
+    return;
+  }
+
+  try {
+    if (data ['soap:Envelope'] ['soap:Body'] ['soap:Fault'] .faultcode) {
+      error = new Error ('API error');
+      error.api = {
+        code: data ['soap:Envelope'] ['soap:Body'] ['soap:Fault'] .faultcode,
+        message: data ['soap:Envelope'] ['soap:Body'] ['soap:Fault'] .faultstring
+      };
+
+      callback (error);
+      return;
+    }
+  } catch (e) {
+    // skip
+  }
+
+  // all good
+  callback (null, data);
+}
+
+
+/**
+ * Communication with API
+ *
+ * @param method {string} - Part of request path `/ns-api-METHOD_NAME`
+ * @param [params] {object} - Request parameters
+ * @param callback {function} - `function (err, data) {}`
+ */
+
+function talk (method, params, callback) {
+  var i;
   var options = {
-    parameters: props,
-    auth: app.username +':'+ app.password
+    url: 'https://webservices.ns.nl/ns-api-' + method,
+    method: 'GET',
+    timeout: config.timeout,
+    binary: true,
+    auth: config.username + ':' + config.password,
+    headers: {
+      'Accept': 'text/xml; charset=UTF-8',
+      'Accept-Encoding': 'gzip',
+      'User-Agent': 'nsapi.js (https://github.com/fvdm/nodejs-ns-api)'
+    }
   };
 
-  http.get (url, options, function (err, res) {
-    var error = null;
-    var data = res.body;
+  // params is optional
+  if (typeof params === 'function') {
+    callback = params;
+    params = {};
+  }
 
-    if (err) {
-      error = new Error ('request failed');
-      error.details = err;
-    }
-
-    try {
-      data = parsexml (data);
-      data = objectOmit (data, '@');
-      
-      if (data ['soap:Envelop'] ['soap:Body'] ['soap:Fault'] ['faultcode']) {
-        error = new Error ('API error');
-        error.details = {
-          code: data ['soap:Envelop'] ['soap:Body'] ['soap:Fault'] ['faultcode'],
-          message: data ['soap:Envelop'] ['soap:Body'] ['soap:Fault'] ['faultstring']
-        };
-      }
-    }
-    catch (e) {
-      if (!error) {
-        error = new Error ('invalid reponse');
-        error.details = {
-          request: options,
-          response: {
-            headers: response.headers,
-            data: data
-          }
-        };
+  // build parameters
+  if (Object.keys (params) .length) {
+    for (i in params) {
+      if (typeof params [i] === 'boolean') {
+        params [i] = params [i] === true ? 'true' : 'false';
       }
     }
 
-    callback (error, !error && data);
+    options.parameters = params;
+  }
+
+  // do request
+  http.doRequest (options, function (err, res) {
+    processResponse (err, res, callback);
   });
 }
 
+
+/**
+ * Vertrektijden - departure times
+ *
+ * @callback callback
+ * @param station {string} - Station ID
+ * @param callback {function} - `function (err, data) {}`
+ * @returns {void}
+ */
+
+function methodVertrektijden (station, callback) {
+  talk ('avt', { station: station }, function (err, data) {
+    var i;
+
+    if (err) {
+      callback (err);
+      return;
+    }
+
+    if (!data.ActueleVertrekTijden || !data.ActueleVertrekTijden.VertrekkendeTrein) {
+      callback (new Error ('unexpected response'));
+      return;
+    }
+
+    data = data.ActueleVertrekTijden.VertrekkendeTrein;
+
+    if (data.RitNummer) {
+      data = [data];
+      callback (null, data);
+      return;
+    }
+
+    for (i in data) {
+      data [i] .VertrekSpoorWijziging = data [i] .VertrekSpoor.wijziging === 'true' ? true : false;
+      data [i] .VertrekSpoor = data [i] .VertrekSpoor ['@text'];
+    }
+
+    callback (null, data);
+  });
+}
+
+
+/**
+ * Prijzen - tariffs
+ *
+ * @callback callback
+ * @param params {object} - Parameters
+ * @param callback {function} - `function (err, data) {}`
+ * @returns {void}
+ */
+
+function methodPrijzen (params, callback) {
+  talk ('prijzen-v3', params, callback);
+}
+
+
+/**
+ * Reisadvies - travel advise
+ *
+ * @callback callback
+ * @param params {object} - Parameters
+ * @param callback {function} - `function (err, data) {}`
+ * @returns {void}
+ */
+
+function methodReisadvies (params, callback) {
+  talk ('treinplanner', params, function (err, data) {
+    var reis;
+    var deel;
+    var stop;
+    var r;
+    var d;
+    var s;
+
+    if (err) {
+      callback (err);
+      return;
+    }
+
+    if (!data.ReisMogelijkheden || !data.ReisMogelijkheden.ReisMogelijkheid) {
+      callback (new Error ('unexpected response'));
+      return;
+    }
+
+    data = data.ReisMogelijkheden.ReisMogelijkheid;
+
+    if (!(data instanceof Array)) {
+      data = [data];
+    }
+
+    if (data.length) {
+      for (r in data) {
+        reis = data [r];
+
+        if (!(reis.ReisDeel instanceof Array)) {
+          reis.ReisDeel = [reis.ReisDeel];
+        }
+
+        for (d in reis.ReisDeel) {
+          deel = reis.ReisDeel [d];
+
+          for (s in deel.ReisStop) {
+            stop = deel.ReisStop [s];
+
+            if (stop.Spoor) {
+              stop.SpoorWijziging = stop.Spoor.wijziging === 'true' ? true : false;
+              stop.Spoor = stop.Spoor ['@text'];
+              deel.ReisStop [s] = stop;
+            }
+          }
+
+          reis.ReisDeel [d] = deel;
+        }
+
+        data [r] = reis;
+      }
+    }
+
+    callback (null, data);
+  });
+}
+
+
+/**
+ * List available stations
+ *
+ * @callback callback
+ * @param [treeKey] {string} - 
+ * @param callback {function} - `function (err, data) {}`
+ */
+
+function methodStations (treeKey, callback) {
+  if (typeof treeKey === 'function') {
+    callback = treeKey;
+    treeKey = 'code';
+  }
+
+  talk ('stations-v2', function (err, data) {
+    var tree = {};
+    var station;
+    var s;
+
+    if (err) {
+      callback (err);
+      return;
+    }
+
+    if (treeKey === false) {
+      tree = [];
+    }
+
+    if (!data.Stations.Station) {
+      callback (new Error ('unexpected response'));
+      return;
+    }
+
+    data = data.Stations.Station;
+
+    for (s in data) {
+      station = data [s];
+      station.Synoniemen = station.Synoniemen && station.Synoniemen.Synoniem || [];
+
+      if (typeof station.Synoniemen === 'string') {
+        station.Synoniemen = [station.Synoniemen];
+      }
+
+      if (treeKey === false) {
+        tree.push (station);
+        break;
+      }
+
+      if (treeKey === 'code') {
+        tree [station.Code] = station;
+      } else if (!station [treeKey]) {
+        callback (new Error ('key not found in station'));
+        return;
+      } else {
+        if (!tree [station [treeKey]]) {
+          tree [station [treeKey]] = {};
+        }
+
+        tree [station [treeKey]] [station.Code] = station;
+      }
+    }
+
+    callback (null, tree);
+  });
+}
+
+
+/**
+ * List disruptions
+ *
+ * @callback callback
+ * @param params {object} - Parameters
+ * @param callback {function} - `function (err, data) {}`
+ */
+
+function methodStoringen (params, callback) {
+  if (typeof params === 'function') {
+    callback = params;
+    params = {
+      actual: true
+    };
+  }
+
+  talk ('storingen', params, function (err, data) {
+    if (err) {
+      callback (err);
+      return;
+    }
+
+    if (typeof data.Storingen.Ongepland === 'string') {
+      data.Storingen.Ongepland = [data.Storingen.Ongepland];
+    }
+
+    if (typeof data.Storingen.Gepland === 'string') {
+      data.Storingen.Gepland = [data.Storingen.Gepland];
+    }
+
+    if (!data.Storingen.Ongepland || !data.Storingen.Gepland) {
+      callback (new Error ('unexpected response'));
+      return;
+    }
+
+    data = data.Storingen;
+    data.Ongepland = data.Ongepland.Storing || [];
+    data.Gepland = data.Gepland.Storing || [];
+
+    if (!(data.Ongepland instanceof Array)) {
+      data.Ongepland = [data.Ongepland];
+    }
+
+    if (!(data.Gepland instanceof Array)) {
+      data.Gepland = [data.Gepland];
+    }
+
+    callback (null, data);
+  });
+}
+
+
+// Setup
+function setup (username, password, timeout) {
+  config.username = username;
+  config.password = password;
+  config.timeout = timeout || config.timeout;
+
+  return {
+    vertrektijden: methodVertrektijden,
+    reisadvies: methodReisadvies,
+    prijzen: methodPrijzen,
+    stations: methodStations,
+    storingen: methodStoringen
+  };
+}
+
 // ready
-module.exports = app;
+module.exports = setup;
