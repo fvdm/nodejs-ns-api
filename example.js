53843cc9ecd4c2f18d379ba78fd288d50c91e105
Franklin van de Meent <fr@nkl.in>
2019-05-10 19:06:43 +0200

    Chore(example): Rewrite to v3 interface

--- example.js
+++ example.js
@@ -1,21 +1,24 @@
-const ns = require ('ns-api') ({
-  username: 'api-username',
-  password: 'api-password'
+const NSAPI = require ('ns-api');
+const ns = new NSAPI ({
+  key: 'abc123',
 });
+
+// console.log does not log >3 levels
+function output (data) {
+  console.dir (data, {
+    depth: null,
+    colors: true,
+  });
+}
 
 // Travel parameters
 const params = {
   fromStation: 'Amersfoort',
-  toStation: 'Den Haag'
+  toStation: 'Den Haag',
 };
 
-// console.log does not log >3 levels
-function myCallback (err, data) {
-  console.dir (err || data, {
-    depth: null,
-    colors: true
-  });
-}
-
 // Get travel advise
-ns.reisadvies (params, myCallback);
+ns.trips (params)
+  .then (output)
+  .catch (console.error)
+;
